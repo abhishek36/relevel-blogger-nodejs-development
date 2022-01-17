11244b0936b296cf6e379869742376ad824d639e
abhishek36 <akamble9913@gmail.com>
2022-01-17 12:23:11 +0530

    first commit

---
+++ src/controllers/post.controller.js
@@ -0,0 +1,30 @@
+//import Post model here
+
+const addPost = async (req, res) => {
+  //addPost api logic here
+};
+
+const getPostByBlogId = async (req, res) => {
+  //getPostByBlogId api logic here
+};
+
+const getPostById = async (req, res) => {
+  //getPostById api logic here
+};
+
+const updatePostById = async (req, res) => {
+  //updatePostById api logic here
+};
+
+const deletePostById = async (req, res) => {
+  //deletePostById api logic here
+};
+
+const PostController = {
+  addPost,
+  getPostByBlogId,
+  getPostById,
+  updatePostById,
+  deletePostById,
+};
+module.exports = PostController;
